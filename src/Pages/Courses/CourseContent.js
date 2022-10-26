fecc12add805b8c2a1902faa4e710f0e777bc18b
mitaly-dev <mitalyy.dev@gmail.com>
2022-10-26 09:18:33 -0700

    course title updated

--- src/Pages/Courses/CourseContent.js
+++ src/Pages/Courses/CourseContent.js
@@ -8,7 +8,7 @@ const CourseContent = () => {
     const content = 'Here are some of our popular courses, and most of our students are successful today by doing these courses. Now these courses are in demand all over the world.'
     return (
         <div>
-            <Title title="Polular courses" content={content}></Title>
+            <Title title="Popular Courses" content={content}></Title>
             <div className='sm:grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-7'>
                 {
                     courses.map(course=><CourseCard key={course.id} course={course}></CourseCard>)
